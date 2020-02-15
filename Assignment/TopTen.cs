b74ff5d536c1397ad417435116629bca0b828f22
Jongbloed <erik.jongbloed@gmail.com>
2020-02-15 22:00:30 +0100

    rename

--- Assignment/TopTen.cs
+++ Assignment/TopTen.cs
@@ -14,7 +14,7 @@ namespace Assignment
     {
         private readonly Dictionary<int, Makelaar> makelaars = new Dictionary<int, Makelaar>();
 
-        public void AddListings(WoonObject[] woonObjecten)
+        public void AddWoonObjecten(WoonObject[] woonObjecten)
         {
             foreach (var woonObject in woonObjecten)
             {
