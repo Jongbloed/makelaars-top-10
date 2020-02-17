ab7a308fc33a3fc45c2c49937427342b4a8967ee
Jongbloed <erik.jongbloed@gmail.com>
2020-02-17 15:31:06 +0100

    give exceptions their own file

---
+++ Assignment/Exceptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace Assignment
+{
+    class RequestLimitExceededException : Exception
+    {
+        public RequestLimitExceededException(string reasonPhrase)
+            : base(reasonPhrase)
+        { }
+    }
+
+    public class UnexpectedApiResponseException : Exception
+    {
+        public UnexpectedApiResponseException(string message)
+            : base(message)
+        { }
+
+        public UnexpectedApiResponseException(Exception innerException)
+            : base("Failed to parse response from API", innerException)
+        { }
+
+        public UnexpectedApiResponseException(HttpStatusCode statusCode, string reasonPhrase) 
+            : base($"Unexpected status code returned from API: [{statusCode}] {reasonPhrase}")
+        { }
+    }
+}
--- Assignment/Fetcher.cs
+++ Assignment/Fetcher.cs
@@ -18,28 +18,6 @@ namespace Assignment
         Task<FundaResultaat> HaalPagina(int pagina, CancellationToken cancellationToken);
     }
 
-    class RequestLimitExceededException : Exception
-    {
-        public RequestLimitExceededException(string reasonPhrase)
-            : base(reasonPhrase)
-        { }
-    }
-
-    public class UnexpectedApiResponseException : Exception
-    {
-        public UnexpectedApiResponseException(string message)
-            : base(message)
-        { }
-
-        public UnexpectedApiResponseException(Exception innerException)
-            : base("Failed to parse response from API", innerException)
-        { }
-
-        public UnexpectedApiResponseException(HttpStatusCode statusCode, string reasonPhrase) 
-            : base($"Unexpected status code returned from API: [{statusCode}] {reasonPhrase}")
-        { }
-    }
-
     public interface IApiClient : IDisposable
     {
         Task<HttpResponseMessage> GetAsync(string url, CancellationToken cancellationToken);
